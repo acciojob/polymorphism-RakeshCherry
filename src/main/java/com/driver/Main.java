9444c48e7b8117ce377a3d66a5cbc01cd7bdcb42
RAKESH M B <rakeshbhavimani1@gmail.com>
2023-07-19 12:35:08 +0530

    Assignment done R

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,32 @@ package com.driver;
 
 public class Main {
 
+    public static void main (String[] args) {
+
+        Product p = new Product();
+
+        int two = p.product(10, 20);
+
+        int three = p.product(10,20,30);
+
+        double twoDouble = p.product(10,30);
+
+        System.out.println(two + " " + three + " " + twoDouble);
+    }
+
+        public static class Product {
+
+            public int product(int x, int y) {
+                return x * y;
+            }
+
+            public int product(int x, int y, int z){
+                return x*y*z;
+            }
+
+            public double product(double x, double y){
+                return x*y;
+            }
+        }
+
 }
